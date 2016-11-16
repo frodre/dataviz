910d8a120f2aadde49c9db5fd5722daa0f612111
Phoebe Merritt <pmerritt@spatialdev.com>
2016-11-16 15:17:35 -0800

    add basic time slider

--- dataviz/static/js/app.js
+++ dataviz/static/js/app.js
@@ -82,3 +82,56 @@ d3.request("/raster/1")
           context.stroke();
         });
   });
+    });
+
+
+// time slider (code modified from: https://bl.ocks.org/mbostock/6452972)
+
+// min/max timeslider values
+var min = 0, max = 100;
+
+var svg = d3.select("svg"),
+    margin = {right: 50, left: 50},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height");
+
+var x = d3.scaleLinear()
+    .domain([min, max])
+    .range([0, width])
+    .clamp(true);
+
+var slider = svg.append("g")
+    .attr("class", "slider")
+    .attr("transform", "translate(" + margin.left + "," + height / 2 + ")");
+
+slider.append("line")
+    .attr("class", "track")
+    .attr("x1", x.range()[0])
+    .attr("x2", x.range()[1])
+    .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .attr("class", "track-inset")
+    .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .attr("class", "track-overlay")
+    .call(d3.drag()
+        .on("start.interrupt", function() { slider.interrupt(); })
+        .on("start drag", function() { hue(x.invert(d3.event.x)); }));
+
+slider.insert("g", ".track-overlay")
+    .attr("class", "ticks")
+    .attr("transform", "translate(0," + 18 + ")")
+    .selectAll("text")
+    .data(x.ticks(10))
+    .enter().append("text")
+    .attr("x", x)
+    .attr("text-anchor", "middle")
+    .text(function(d) { return d; });
+
+// add slider handle element
+var handle = slider.insert("circle", ".track-overlay")
+    .attr("class", "handle")
+    .attr("r", 9);
+
+function hue(h) {
+    handle.attr("cx", x(h));
+}
+>>>>>>> add basic time slider:app.js

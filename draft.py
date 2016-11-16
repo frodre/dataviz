5ac45071cf5292acbba2626fe0923c1d453d9281
John Dwyer <jgdwyer@gmail.com>
2016-11-15 16:59:42 -0800

    Added a rough pseudocode file to outline some tasks

---
+++ draft.py
@@ -0,0 +1,17 @@
+#----- Pseudocode for the backend part of the project -----#
+
+#import packages: xarray....
+
+# Read in one netcdf file (in the future we can add a whole directory of files)
+
+# Process the file (extract the variable of interest, etc.)
+
+# Convert file to GeoTiff format
+
+# Set up flask or django
+
+# Pass file to d3.js
+
+# Do frontend magic
+
+# Provide an option to save movie to file

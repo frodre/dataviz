9245463f983e65b43a96e0323f753f348c345e70
Andre Perkins <frodre@gmail.com>
2016-11-18 09:45:37 -0800

    Removed some extraneous code from rebase.

--- dataviz/static/js/app.js
+++ dataviz/static/js/app.js
@@ -3,18 +3,6 @@
 var width = 800,
     height = 500;
 
-// append an HTML5 canvas element to the map div
-var canvas = d3.select("#map").append("canvas")
-    .attr("width", width)
-    .attr("height", height);
-var context = canvas.node().getContext("2d");
-
-// define projection
-var projection = d3.geoEquirectangular();
-var path = d3.geoPath()
-    .projection(projection)
-    .context(context);
-
 var years = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 // get tiff for each year
